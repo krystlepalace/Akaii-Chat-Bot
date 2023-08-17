9e02df00aaae2b2dcf1f87838e3918bd343a392e
Fvbvke <novoselcevila12@gmail.com>
2023-08-17 22:08:46 +0300

    Added query for new keyboard

--- handlers/callback_query.py
+++ handlers/callback_query.py
@@ -31,3 +31,29 @@ async def process_anim_toggle_callback(callback: CallbackQuery):
                 )
         await callback.answer()
 
+
+@router.callback_query()
+async def process_voice_toggle_callback(callback: CallbackQuery):
+    if await group.isAdmin(callback.message, callback.from_user.id):
+        bot_member = await callback.message.chat.get_member(user_id=callback.message.from_user.id)
+        if bot_member.status == ChatMemberStatus.ADMINISTRATOR:
+            chat = db_chat.Chat(callback.message.chat.id)
+
+            if callback.data == 'voice0':
+                chat.set_voice(True)
+                await callback.message.answer("Перевод голосвых сообщений включен.")
+                await callback.answer()
+            if callback.data == 'voice1':
+                chat.set_voice(False)
+                await callback.message.answer("Перевод голосовых сообщений отключен.")
+                await callback.answer()
+        else:
+            await callback.message.answer(
+                    "Бот не является администратором."
+                    )
+    else:
+        await callback.message.answer(
+                "Вы не являетесь администратором."
+                )
+        await callback.answer()
+
