dab922304b4009a1179ce46e18d706c630aa2b14
Fvbvke <novoselcevila12@gmail.com>
2023-08-19 01:16:36 +0300

    Update database.py

--- models/database.py
+++ models/database.py
@@ -5,7 +5,7 @@ class Database:
     __instance = None
     
     def __init__(self, redis_url):
-        self.r = redis.Redis(redis_url, charset="utf-8", decode_responses=True)
+        self.r = redis.Redis(host=redis_url, port=6379, charset="utf-8", decode_responses=True)
 
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
