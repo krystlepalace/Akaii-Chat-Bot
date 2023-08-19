451c59fd50d06dda0e6e28a1408c00f4325f0ec1
Fvbvke <novoselcevila12@gmail.com>
2023-08-19 19:17:26 +0300

    Update commands.py
    Made separate menus for users and admins

--- utils/commands.py
+++ utils/commands.py
@@ -1,13 +1,16 @@
 from aiogram import Bot
-from aiogram.types import BotCommand, BotCommandScopeDefault
+from aiogram.types import BotCommand, BotCommandScopeDefault, BotCommandScopeAllChatAdministrators
 
 
 async def set_commands(bot: Bot):
-    commands = [
+    default_commands = [
             BotCommand(
                 command="help",
                 description="Помощь по боту"
                 ),
+                ]
+
+    admin_commands = [
             BotCommand(
                 command="animations",
                 description="Включить или отключить анимированные стикеры"
@@ -15,8 +18,25 @@ async def set_commands(bot: Bot):
             BotCommand(
                 command="voice",
                 description="Включить или отключить перевод ГС"
+                ),
+            BotCommand(
+                command="ban",
+                description="Заблокировать пользователя"
+                ),
+            BotCommand(
+                command="unban",
+                description="Разблокировать пользователя"
+                ),
+            BotCommand(
+                command="mute",
+                description="Заглушить пользователя"
+                ),
+            BotCommand(
+                command="unmute",
+                description="Вернуть право голоса"
                 )
-            ]
+                ]
 
-    await bot.set_my_commands(commands, BotCommandScopeDefault())
+    await bot.set_my_commands(default_commands, BotCommandScopeDefault())
+    await bot.set_my_commands(admin_commands, BotCommandScopeAllChatAdministrators())
 
