d7b12765b46e178d31bc1c498fce75b6bb8c69da
Fvbvke <novoselcevila12@gmail.com>
2023-08-24 23:11:34 +0300

    Formatting

--- filters/group.py
+++ filters/group.py
@@ -2,14 +2,6 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.enums.chat_member_status import ChatMemberStatus
 from aiogram.filters import BaseFilter
 
-# Just checking if user has admin priveleges without notifying if he has'nt
-#class IsAdmin(BaseFilter):
-#    async def __call__(self, message: Message):
-#        chat_member = await message.chat.get_member(user_id=message.from_user.id)
-#        return (
-#                chat_member.status == ChatMemberStatus.ADMINISTRATOR
-#                or chat_member.status == ChatMemberStatus.CREATOR
-#                )
 
 # Bot will notify if user is not an admin
 class IsAdmin(BaseFilter):
@@ -22,17 +14,6 @@ class IsAdmin(BaseFilter):
             await message.reply("Вы не являетесь администратором.")
             return False
 
-
-# Just checking if user has admin priveleges without notifying if he has'nt
-#class IsAdminCallback(BaseFilter):
-#    async def __call__(self, callback: CallbackQuery):
-#        chat_member = await callback.message.chat.get_member(
-#                user_id=callback.from_user.id
-#                )
-#        return (
-#                chat_member.status == ChatMemberStatus.ADMINISTRATOR
-#                or chat_member.status == ChatMemberStatus.CREATOR
-#                )
 
 # Bot will notify if user is not an admin
 class IsAdminCallback(BaseFilter):
@@ -50,3 +31,24 @@ class IsAdminCallback(BaseFilter):
                 )
             return False
 
+
+# Just checking if user has admin priveleges without notifying if he has'nt
+#class IsAdmin(BaseFilter):
+#    async def __call__(self, message: Message):
+#        chat_member = await message.chat.get_member(user_id=message.from_user.id)
+#        return (
+#                chat_member.status == ChatMemberStatus.ADMINISTRATOR
+#                or chat_member.status == ChatMemberStatus.CREATOR
+#                )
+
+
+# Just checking if user has admin priveleges without notifying if he has'nt
+#class IsAdminCallback(BaseFilter):
+#    async def __call__(self, callback: CallbackQuery):
+#        chat_member = await callback.message.chat.get_member(
+#                user_id=callback.from_user.id
+#                )
+#        return (
+#                chat_member.status == ChatMemberStatus.ADMINISTRATOR
+#                or chat_member.status == ChatMemberStatus.CREATOR
+#                )
