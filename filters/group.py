4a289194b5d633bd404e374b5f0ad497729721c9
Fvbvke <novoselcevila12@gmail.com>
2023-08-24 21:19:51 +0300

    Update group.py
    Created IsAdminCallback for checking admin priveleges for collbacks

--- filters/group.py
+++ filters/group.py
@@ -1,22 +1,22 @@
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.enums.chat_member_status import ChatMemberStatus
 from aiogram.filters import BaseFilter
-
-
-async def isAdmin(message: Message, user_id=None):
-    if user_id is None:
-        user_id = message.from_user.id
-
-    member = await message.chat.get_member(user_id=user_id)
-    return (
-        member.status == ChatMemberStatus.ADMINISTRATOR
-        or member.status == ChatMemberStatus.CREATOR
-    )
 
 
 class IsAdmin(BaseFilter):
     async def __call__(self, message: Message):
         chat_member = await message.chat.get_member(user_id=message.from_user.id)
+        return (
+                chat_member.status == ChatMemberStatus.ADMINISTRATOR
+                or chat_member.status == ChatMemberStatus.CREATOR
+                )
+
+
+class IsAdminCallback(BaseFilter):
+    async def __call__(self, callback: CallbackQuery):
+        chat_member = await callback.message.chat.get_member(
+                user_id=callback.from_user.id
+                )
         return (
                 chat_member.status == ChatMemberStatus.ADMINISTRATOR
                 or chat_member.status == ChatMemberStatus.CREATOR
