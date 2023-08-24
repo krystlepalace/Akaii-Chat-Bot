31cae6ac541ef8840b5c799eaaa1b5fdfea9b329
Fvbvke <novoselcevila12@gmail.com>
2023-08-24 21:10:53 +0300

    Added normal filter for IsAdmin

--- filters/group.py
+++ filters/group.py
@@ -1,9 +1,10 @@
 from aiogram.types import Message
 from aiogram.enums.chat_member_status import ChatMemberStatus
+from aiogram.filters import BaseFilter
 
 
 async def isAdmin(message: Message, user_id=None):
-    if user_id == None:
+    if user_id is None:
         user_id = message.from_user.id
 
     member = await message.chat.get_member(user_id=user_id)
@@ -11,3 +12,13 @@ async def isAdmin(message: Message, user_id=None):
         member.status == ChatMemberStatus.ADMINISTRATOR
         or member.status == ChatMemberStatus.CREATOR
     )
+
+
+class IsAdmin(BaseFilter):
+    async def __call__(self, message: Message):
+        chat_member = await message.chat.get_member(user_id=message.from_user.id)
+        return (
+                chat_member.status == ChatMemberStatus.ADMINISTRATOR
+                or chat_member.status == ChatMemberStatus.CREATOR
+                )
+
