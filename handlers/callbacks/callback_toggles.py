bf663614d8bc423ba2242369731d6a01c5be06e6
Fvbvke <novoselcevila12@gmail.com>
2023-08-24 23:23:16 +0300

    Restructuring

--- handlers/callbacks/callback_toggles.py
+++ handlers/callbacks/callback_toggles.py
@@ -1,14 +1,15 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
 from aiogram.enums.chat_member_status import ChatMemberStatus
+from aiogram import F
 from models import db_chat
 from filters import group
 
 router = Router()
 
 
-@router.callback_query(group.IsAdminCallback())
-async def process_toggle_callback(callback: CallbackQuery):
+@router.callback_query(F.data.startswith("anim"), group.IsAdminCallback())
+async def process_toggle_anim(callback: CallbackQuery):
     bot_member = await callback.message.chat.get_member(
             user_id=callback.message.from_user.id
         )
@@ -23,6 +24,18 @@ async def process_toggle_callback(callback: CallbackQuery):
             chat.set_anim(False)
             await callback.message.edit_text("Анимированные стикеры запрещены.")
             await callback.answer()
+    else:
+        await callback.message.edit_text("Бот не является администратором.")
+
+
+@router.callback_query(F.data.startswith("voice"), group.IsAdminCallback())
+async def process_toggle_voice(callback: CallbackQuery):
+    bot_member = await callback.message.chat.get_member(
+            user_id=callback.message.from_user.id
+        )
+    if bot_member.status == ChatMemberStatus.ADMINISTRATOR:
+        chat = db_chat.Chat(callback.message.chat.id)
+
         if callback.data == "voice0":
             chat.set_voice(True)
             await callback.message.edit_text("Перевод голосвых сообщений включен.")
@@ -34,4 +47,3 @@ async def process_toggle_callback(callback: CallbackQuery):
 
     else:
         await callback.message.edit_text("Бот не является администратором.")
-
