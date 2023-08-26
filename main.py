f963412e25052446604793c0fa0997bfeda91886
Fvbvke <novoselcevila12@gmail.com>
2023-08-27 01:01:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+from middlewares.throttling import AntiFloodMiddleware
 from handlers import base, administrative, content_filters
 from handlers.callbacks import callback_settings, callback_toggles
 from utils.commands import set_commands
 from models import database
 from config import CONFIG
 
-
+storage = RedisStorage.from_url(CONFIG.redis_url)
 db = database.Database(mongodb_url=CONFIG.mongodb.get_secret_value())
 bot = Bot(token=CONFIG.bot_token.get_secret_value())
 
@@ -14,6 +16,8 @@ bot = Bot(token=CONFIG.bot_token.get_secret_value())
 # Bot startup function
 async def main():
     dp = Dispatcher()
+
+    # Setup routers
     dp.include_routers(
         base.router,
         callback_settings.router,
@@ -21,6 +25,9 @@ async def main():
         administrative.router,
         content_filters.router,
     )
+
+    # Setup middlewares
+    dp.message.middleware.register(AntiFloodMiddleware(storage=storage))
 
     # set commands
     await set_commands(bot)
