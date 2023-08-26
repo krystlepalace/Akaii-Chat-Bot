af2ce2bebb10ce6c8fdac40b3dba43bd7c257bac
Fvbvke <novoselcevila12@gmail.com>
2023-08-26 22:34:48 +0300

    MongoDB methods and auto-deleting messages after some time

--- handlers/callbacks/callback_settings.py
+++ handlers/callbacks/callback_settings.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.types import CallbackQuery
 from keyboards.toggles import voice_inline, anim_inline
 from aiogram import F
+from asyncio import sleep
 from filters import group
 
 router = Router()
@@ -13,8 +14,11 @@ async def proccess_settings_menu(callback: CallbackQuery):
         await callback.message.edit_text("Разрешить анимированные стикеры?")
         await callback.message.edit_reply_markup("s", anim_inline())
         await callback.answer()
+        await sleep(10)
+        await callback.message.delete()
     if callback.data == "settings_voice":
         await callback.message.edit_text("Разрешить перевод ГС в текст?")
         await callback.message.edit_reply_markup("s", voice_inline())
         await callback.answer()
-
+        await sleep(10)
+        await callback.message.delete()
--- handlers/callbacks/callback_toggles.py
+++ handlers/callbacks/callback_toggles.py
@@ -2,8 +2,9 @@ from aiogram import Router
 from aiogram.types import CallbackQuery
 from aiogram.enums.chat_member_status import ChatMemberStatus
 from aiogram import F
-from models import db_chat
+from asyncio import sleep
 from filters import group
+import main
 
 router = Router()
 
@@ -11,19 +12,20 @@ router = Router()
 @router.callback_query(F.data.startswith("anim"), group.IsAdminCallback())
 async def process_toggle_anim(callback: CallbackQuery):
     bot_member = await callback.message.chat.get_member(
-            user_id=callback.message.from_user.id
-        )
+        user_id=callback.message.from_user.id
+    )
     if bot_member.status == ChatMemberStatus.ADMINISTRATOR:
-        chat = db_chat.Chat(callback.message.chat.id)
-
         if callback.data == "anim0":
-            chat.set_anim(True)
-            await callback.message.edit_text("Аминированные стикеры разрешены!")
+            await main.db.set_anim(callback.message.chat.id, True)
+            await callback.message.edit_text("Анимированные стикеры разрешены!")
             await callback.answer()
         if callback.data == "anim1":
-            chat.set_anim(False)
+            await main.db.set_anim(callback.message.chat.id, False)
             await callback.message.edit_text("Анимированные стикеры запрещены.")
             await callback.answer()
+
+        await sleep(10)
+        await callback.message.delete()
     else:
         await callback.message.edit_text("Бот не является администратором.")
 
@@ -31,19 +33,19 @@ async def process_toggle_anim(callback: CallbackQuery):
 @router.callback_query(F.data.startswith("voice"), group.IsAdminCallback())
 async def process_toggle_voice(callback: CallbackQuery):
     bot_member = await callback.message.chat.get_member(
-            user_id=callback.message.from_user.id
-        )
+        user_id=callback.message.from_user.id
+    )
     if bot_member.status == ChatMemberStatus.ADMINISTRATOR:
-        chat = db_chat.Chat(callback.message.chat.id)
-
         if callback.data == "voice0":
-            chat.set_voice(True)
-            await callback.message.edit_text("Перевод голосвых сообщений включен.")
+            await main.db.set_voice(callback.message.chat.id, True)
+            await callback.message.edit_text("Перевод голосовых сообщений включен.")
             await callback.answer()
         if callback.data == "voice1":
-            chat.set_voice(False)
+            await main.db.set_voice(callback.message.chat.id, False)
             await callback.message.edit_text("Перевод голосовых сообщений отключен.")
             await callback.answer()
 
+        await sleep(10)
+        await callback.message.delete()
     else:
         await callback.message.edit_text("Бот не является администратором.")
