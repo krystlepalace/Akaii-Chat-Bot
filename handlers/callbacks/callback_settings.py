b4b46bf6b651ea4a15de7033014c67b42ea76271
Fvbvke <novoselcevila12@gmail.com>
2023-08-28 02:44:47 +0300

    Refactor

--- handlers/callbacks/callback_settings.py
+++ handlers/callbacks/callback_settings.py
@@ -11,15 +11,18 @@ router = Router()
 @router.callback_query(F.data.startswith("settings_"), group.IsAdminCallback())
 async def proccess_settings_menu(callback: CallbackQuery):
     if callback.data == "settings_anim":
-        await callback.message.edit_text("Разрешить анимированные стикеры?")
-        await callback.message.edit_reply_markup("s", anim_inline())
+        await callback.message.edit_text("Разрешить анимированные стикеры?",
+                                         reply_markup=anim_inline())
         await sleep(10)
-        await callback.message.delete()
         await callback.answer()
-    if callback.data == "settings_voice":
-        await callback.message.edit_text("Разрешить перевод ГС в текст?")
-        await callback.message.edit_reply_markup("s", voice_inline())
-        await sleep(10)
         await callback.message.delete()
+    if callback.data == "settings_voice":
+        await callback.message.edit_text("Разрешить перевод ГС в текст?", 
+                                         reply_markup=voice_inline())
+        await sleep(10)
+        await callback.answer()
+        await callback.message.delete()
+    if callback.data == "settings_close":
+        await callback.message.edit_text("Настройки закрыты.")
         await callback.answer()
 
