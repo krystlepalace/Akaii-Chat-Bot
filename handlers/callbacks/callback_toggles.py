c150fc2dda15086afb7f577d12257280dbb59e5c
Fvbvke <novoselcevila12@gmail.com>
2023-08-30 20:00:00 +0300

    New callback for nsfw parameter

--- handlers/callbacks/callback_toggles.py
+++ handlers/callbacks/callback_toggles.py
@@ -38,4 +38,15 @@ async def process_toggle_voice(callback: CallbackQuery, callback_data: ToggleCal
             "Перевод ГС в текст " + callback_data.desc, reply_markup=keyboards.toggles.close()
         )
     await callback.answer()
- 
+
+
+@router.callback_query(
+    ToggleCallback.filter(F.toggle.startswith("nsfw")), group.IsAdminCallback()
+)
+async def process_toggle_nsfw(callback: CallbackQuery, callback_data: ToggleCallback): 
+    await main.db.set_nsfw(callback.message.chat.id, callback_data.status)
+    await callback.message.edit_text(
+            "NSFW " + callback_data.desc, reply_markup=keyboards.toggles.close()
+        )
+    await callback.answer()
+
