0db60a1319b7bfc4d4a21150fb5f8ba3fcbd39d4
Fvbvke <novoselcevila12@gmail.com>
2023-08-30 20:00:27 +0300

    Deleted some questionable code

--- handlers/callbacks/callback_settings.py
+++ handlers/callbacks/callback_settings.py
@@ -1,10 +1,9 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
-from keyboards.toggles import voice_inline, anim_inline, settings_inline
+from keyboards.toggles import nsfw_inline, voice_inline, anim_inline
 from aiogram import F
-from asyncio import sleep
 from filters import group
-import main
+
 
 router = Router()
 
@@ -13,17 +12,15 @@ router = Router()
 async def proccess_settings_menu(callback: CallbackQuery):
     if callback.data == "settings_anim":
         await callback.message.edit_text("Разрешить анимированные стикеры?",
-                                         reply_markup=anim_inline())
-        await sleep(10)
-        await callback.answer()
-        await callback.message.delete()
+                                         reply_markup=anim_inline()) 
     if callback.data == "settings_voice":
         await callback.message.edit_text("Разрешить перевод ГС в текст?", 
                                          reply_markup=voice_inline())
-        await sleep(10)
-        await callback.answer()
-        await callback.message.delete()
+    if callback.data == "settings_nsfw":
+        await callback.message.edit_text("Разрешить NSFW?",
+                                         reply_markup=nsfw_inline())
     if callback.data == "settings_close":
-        await callback.message.edit_text("Настройки закрыты.")
-        await callback.answer()
+        await callback.message.edit_text("Настройки закрыты.",
+                                         reply_markup=None)
+    await callback.answer()
 
