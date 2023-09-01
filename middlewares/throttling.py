d5a817c5228939f6f05e066bf0e1abd592bb37c0
Fvbvke <novoselcevila12@gmail.com>
2023-09-02 01:56:10 +0300

    New middleware for anti-flood

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -2,22 +2,24 @@ from typing import Any, Awaitable, Callable, Dict
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
 from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.types import Message, TelegramObject
+import main
+from handlers.administrative import mute
 
 
-class AntiFloodMiddleware(BaseMiddleware):
+class ThrottlingMiddleware(BaseMiddleware):
     def __init__(self, storage: RedisStorage):
         self.storage = storage
 
-
-    async def __call__(self,
-                       handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-                       event: Message,
-                       data: Dict[str, Any]
-                       ) -> Any:
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any],
+    ) -> Any:
         if event.sticker or event.photo:
             return await handler(event, data)
 
-        user = f'user{event.from_user.id}'
+        user = f"user{event.from_user.id}"
         check_user = await self.storage.redis.get(name=user)
         if check_user:
             if int(check_user.decode()) == 1:
@@ -28,3 +30,33 @@ class AntiFloodMiddleware(BaseMiddleware):
         await self.storage.redis.set(name=user, value=1, ex=3)
         return await handler(event, data)
 
+
+class AntiFloodMiddleware(BaseMiddleware):
+    def __init__(self, storage: RedisStorage):
+        self.storage = storage
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any],
+    ) -> Any:
+        chat = await main.db.get_chat(event.chat.id)
+        if chat.get("antiflood"):
+            user = f"user{event.from_user.id}"
+            check_user = await self.storage.redis.get(name=user)
+            if check_user:
+                if int(check_user.decode()) < 7:
+                    await self.storage.redis.set(
+                        name=user, value=check_user.decode() + 1, ex=2
+                    )
+                else:
+                    await event.reply(text="/mute 1")
+                    await event.answer(
+                        "Пользователь "
+                        + event.from_user.full_name
+                        + " был заглушен за флуд"
+                    )
+            else:
+                await self.storage.redis.set(name=user, value=1, ex=2)
+        return await handler(event, data)
