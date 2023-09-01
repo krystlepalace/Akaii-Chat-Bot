caf5aadefdc871fad560a764409f902506ea2e9e
Fvbvke <novoselcevila12@gmail.com>
2023-09-02 01:55:42 +0300

    Update callback_settings.py

--- handlers/callbacks/callback_settings.py
+++ handlers/callbacks/callback_settings.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
-from keyboards.toggles import nsfw_inline, voice_inline, anim_inline
+from keyboards.toggles import nsfw_inline, voice_inline, anim_inline, antiflood_inline
 from aiogram import F
 from filters import group
 
@@ -19,6 +19,9 @@ async def proccess_settings_menu(callback: CallbackQuery):
     if callback.data == "settings_nsfw":
         await callback.message.edit_text("Разрешить NSFW?",
                                          reply_markup=nsfw_inline())
+    if callback.data == "settings_antiflood":
+        await callback.message.edit_text("Включить Anti-Flood?",
+                                         reply_markup=antiflood_inline())
     if callback.data == "settings_close":
         await callback.message.edit_text("Настройки закрыты.",
                                          reply_markup=None)
